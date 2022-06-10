8cbd874deb376a293bc573c2ebf7c2190f803c3a
James <james.auble@gmail.com>
2022-06-10 14:05:11 -0700

    completed banner block

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ remove_action('wp_body_open', 'wp_global_styles_render_svg_filters');
 function diesel_features() {
   add_theme_support('editor-styles');
   add_editor_style(['build/style-index.css', 'build/index.css']);
+  add_image_size('pageBanner', 1500, 350, true);
 }
 
 add_action('after_setup_theme', 'diesel_features');
@@ -21,19 +22,38 @@ function diesel_files() {
 add_action('wp_enqueue_scripts', 'diesel_files');
 
 class JSXBlock {
-  public function __construct($name) {
+  public function __construct($name, $renderCallback = null, $data = null) {
     $this->name = $name;
+    $this->data = $data;
+    $this->renderCallback = $renderCallback;
     add_action('init', [$this, 'onInit']);
+  }
+
+  public function ourRenderCallback($attributes, $content) {
+    ob_start();
+    require get_theme_file_path("/diesel-blocks/{$this->name}.php");
+    return ob_get_clean();
   }
 
   public function onInit() {
     wp_register_script($this->name, get_stylesheet_directory_uri() . "/build/{$this->name}.js", ['wp-blocks', 'wp-editor']);
-    register_block_type("dieselblocks/{$this->name}", [
+
+    $ourArgs = [
       'editor_script' => $this->name
-    ]);
+    ];
+
+    if ($this->data) {
+      wp_localize_script($this->name, $this->name, $this->data);
+    }
+
+    if ($this->renderCallback) {
+      $ourArgs['render_callback'] = [$this, 'ourRenderCallback'];
+    }
+
+    register_block_type("dieselblocks/{$this->name}", $ourArgs);
   }
 }
 
-new JSXBlock('banner');
+new JSXBlock('banner', true, ['fallbackImage' => get_theme_file_uri('/images/library-hero.jpg')]);
 new JSXBlock('genericheading');
 new JSXBlock('genericbutton');
